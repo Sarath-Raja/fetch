9d14b358c709a8a082a926d860107f7383dd614e
Sarath Raja <sarathrajahbk@gmail.com>
2021-05-19 01:22:54 +0530

    Initial Commit

---
+++ src/public/fetch.js
@@ -0,0 +1,21 @@
+fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: "Sarath Raja"
+    })
+})
+    .then(res => {
+        // Fetch doesn't fail and go to error block until an network error happens
+        // So we use res.ok to deterimint if the call is success/failure
+        // Status codes 200-299 are considered as success
+        if(res.ok)
+            console.log('Success', res.status)
+        else
+            console.log('Failure', res.status)
+        res.json()
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+    })
